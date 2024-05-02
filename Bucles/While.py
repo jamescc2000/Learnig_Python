894d447042ffbfe7b8b031d6f872b9a74c6bba36
James Contreras <jamescontrerasc458@gmail.com>
2024-05-01 21:12:25 -0500

    Agregando while

--- Bucles/While.py
+++ Bucles/While.py
@@ -1,1 +1,9 @@
 ######################### WHILE #######################
+# El bucle se ejecuta mientras que se cumpla la condicion
+
+contador = 0
+
+while(contador < 10):
+    contador +=1
+    
+print(f'El contador llego a su fin con un valor de {contador}')
