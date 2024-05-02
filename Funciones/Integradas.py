7e58076cda052cb23ea3b5ce04e101f3d63b0fdb
James Contreras <jamescontrerasc458@gmail.com>
2024-05-01 21:36:20 -0500

    Funciones integradas

---
+++ Funciones/Integradas.py
@@ -0,0 +1,56 @@
+######################## FUNCIONES INTEGRADAS ####################3
+################ MAX ##############
+# Se utiliza para encontrar el numero mayor de un arreglo
+# Solo sirve si los valores son numericos, en caso haya algun string, bota error
+numeros = [4, 52, 100, 21, 85, 104]
+
+print(max(numeros))
+
+
+################ MIN ##############
+# Se utiliza para encontrar el numero menor de un arreglo
+# Solo sirve si los valores son numericos, en caso haya algun string, bota error
+numeros = {97,6,18,22,77}
+
+print(min(numeros))
+
+
+############### ROUND ################
+# Sirve para redondear un valor numerico
+numero = 12.4865812
+
+print(round(numero,3)) # Redonde el numeor a 3 decimales
+
+
+############### BOOL ################
+# Devuelve False si recibe como entrada: 0 , vacio/null, False
+# Caso contrario devuelve True
+resultado = bool()
+print(resultado)
+
+resultado = bool(0)
+print(resultado)
+
+resultado = bool(78)
+print(resultado)
+
+resultado = bool('aa')
+print(resultado)
+
+
+############### ALL ################
+# Devuelve True si todos los valores son True
+# Caso contrario devuelve False
+
+resultado = all([12, 87, 45, 78, 'Valor'])
+print(resultado)
+
+resultado = all([12, 0, 45, 78, 'Valor'])
+print(resultado)
+
+
+################### SUM #############
+# Devuelve la suma de todos los valores de un iterable
+
+suma = sum(numeros)
+print(suma)
