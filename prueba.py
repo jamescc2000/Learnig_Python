eb45325e26194eff53f8ff2e1cab545b954dd1cd
James Contreras <jamescontrerasc458@gmail.com>
2024-05-03 11:08:44 -0500

    groupby

--- prueba.py
+++ prueba.py
@@ -1,3 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import groupby
 
-multiplicar = lambda x : x * 10 + x
-print(multiplicar(5))
+numbers = input()
+list_num = list()
+for k, g in groupby(numbers):
+    list_num.append(f'({len(list(g))}, {k})')
+    
+print(' '.join(map(str, list_num)))
