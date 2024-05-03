2cbaa149f7910c15bacedfe69316318d7928c2f0
James Contreras <jamescontrerasc458@gmail.com>
2024-05-03 16:47:13 -0500

    Itertools

--- prueba.py
+++ prueba.py
@@ -1,9 +1,29 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from itertools import groupby
+from itertools import combinations
 
-numbers = input()
-list_num = list()
-for k, g in groupby(numbers):
-    list_num.append(f'({len(list(g))}, {k})')
+n = 6
+text = 'a b c d e a'.split()
+k = 2
+index = list()
+index_a = list()
+
+for i, char in enumerate(text):
+    index.append(i+1)
+    if char == 'a':
+        index_a.append(i+1)
+
+perm = list(combinations(index,k))
+bool = False
+cont = 0
+
+for tuple in perm:
+    for j in tuple:
+        if j in index_a:
+            bool = True
     
-print(' '.join(map(str, list_num)))
+    if bool:
+        cont +=1
+    bool = False
+        
+print(cont/len(perm))
+        
+
