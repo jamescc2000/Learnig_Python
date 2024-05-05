c1a7d4cfacd098aebfcc40aab0ea780e49723622
James Contreras <jamescontrerasc458@gmail.com>
2024-05-05 10:40:21 -0500

    Ejercicios

--- prueba.py
+++ prueba.py
@@ -1,29 +1,28 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 from itertools import combinations
+import math as m
 
-n = 6
-text = 'a b c d e a'.split()
-k = 2
-index = list()
-index_a = list()
 
-for i, char in enumerate(text):
-    index.append(i+1)
-    if char == 'a':
-        index_a.append(i+1)
 
-perm = list(combinations(index,k))
-bool = False
-cont = 0
+K, M = input().split()
+list_lists = list()
+a = m.sqrt(int(M)/3)
+sum_tot = 0
 
-for tuple in perm:
-    for j in tuple:
-        if j in index_a:
-            bool = True
+for i in range(int(K)):
+    lista = list(map(int, input().split()))
+    list_num = lista[1:]
+    list_num.sort()
+    ith = 0
+    if max(list_num) < a:
+        sum_tot += max(list_num)*max(list_num)
+    elif min(list_num) > a:
+        sum_tot += min(list_num)*min(list_num)
+    else:
+        for j in range(2,len(list_num)-3):
+            if list_num[j] <= a and list_num[j+1] > a:
+                sum_tot += list_num[j]*list_num[j]
+        
+        
+print(sum_tot)
     
-    if bool:
-        cont +=1
-    bool = False
-        
-print(cont/len(perm))
-        
-
