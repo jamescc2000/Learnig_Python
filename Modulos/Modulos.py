dc551470b11d359e6519fee599b83e2f45e24f7b
James Contreras <jamescontrerasc458@gmail.com>
2024-05-05 21:33:50 -0500

    Modulos

---
+++ Modulos/Modulos.py
@@ -0,0 +1,7 @@
+from Modulos_saludar import saludar as saludar_normal, saludar_raro
+
+saludo = saludar_normal('James')
+saludo_raro = saludar_raro('Juanito')
+
+print(saludo)
+print(saludo_raro)
---
+++ Modulos/Modulos_saludar.py
@@ -0,0 +1,6 @@
+def saludar(name):
+    return f'Hola {name} espero que hayas tenido un maravilloso dia'
+    
+def saludar_raro(name):
+    return f'Hola {name} espero que este bien asfkjlk xd'
+    
