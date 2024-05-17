5c7803a9b6773144f3d5bbe5398a49df8f87d535
James Contreras <jamescontrerasc458@gmail.com>
2024-05-16 23:08:52 -0500

    HackerRank - Classes

--- prueba.py
+++ prueba.py
@@ -1,17 +1,33 @@
-from itertools import product
+import math
 
-# Supongamos que tienes siete listas de siete elementos cada una
-lista1 = [1, 2, 3, 4, 5, 6, 7]
-lista2 = [8, 9, 10, 11, 12, 13, 14]
-lista3 = [15, 16, 17, 18, 19, 20, 21]
-lista4 = [22, 23, 24, 25, 26, 27, 28]
-lista5 = [29, 30, 31, 32, 33, 34, 35]
-lista6 = [36, 37, 38, 39, 40, 41, 42]
-lista7 = [43, 44, 45, 46, 47, 48, 49]
+class Points(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-# Obtener todas las combinaciones posibles
-combinaciones = list(product(lista1, lista2, lista3, lista4, lista5, lista6, lista7))
+    def __sub__(self, no):
+        if isinstance(no, Points):
+            return Points(self.x - no.x, self.y - no.y, self.z - no.z)
+        return NotImplemented
 
-# Imprimir las combinaciones
-for combinacion in combinaciones:
-    print(combinacion)
+    def dot(self, no):
+        return self.x*no.x + self.y*no.y +  self.z*no.z
+
+    def cross(self, no):
+        return Points(self.y*no.z - self.z*no.y, (self.z*no.x - self.x*no.z), self.x*no.y - self.y*no.x)
+        
+    def absolute(self):
+        return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
+
+if __name__ == '__main__':
+    points = [[0, 4, 5], [1 ,7, 6], [0, 5, 9], [1, 7, 2]]
+
+    a, b, c, d = Points(*points[0]), Points(*points[1]), Points(*points[2]), Points(*points[3])
+    x = (b - a).cross(c - b)
+    y = (c - b).cross(d - c)
+
+    
+    angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
+
+    print("%.2f" % math.degrees(angle))
